53641c2d37d8ff8a9604d836e28c1e482bdbd364
benjisimon <benjisimon@gmail.com>
2014-09-30 09:01:13 -0400

    Renamed the comp function

--- programming-praxis/thue-morse-sequence.scm
+++ programming-praxis/thue-morse-sequence.scm
@@ -1,12 +1,12 @@
 ; http://programmingpraxis.com/2014/09/30/thue-morse-sequence/
 
-(define (comp seq)
+(define (flip seq)
   (map (lambda (x) (if (= x 0) 1 0)) seq))
   
  (define (tms terms)
    (let loop ((i 0) (accum '(0)))
      (if (< i terms)
          (loop (+ 1 i)
-               (append accum (comp accum)))
+               (append accum (flip accum)))
          accum)))
          
