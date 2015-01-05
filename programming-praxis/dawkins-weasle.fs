e5f3cb414f03323fd911841b06f280c6e83d7cf7
default_git_name <default_git_email>
2015-01-05 12:12:07 +0000

    more hacking with forth and life.

--- programming-praxis/dawkins-weasle.fs
+++ programming-praxis/dawkins-weasle.fs
@@ -1,4 +1,8 @@
 require random.fs
+ 
+: between? { x lower upper -- bool }
+ x lower >= and
+ x upper <=  ;
 
 : rand-between { lower upper -- n }
  upper lower -
@@ -24,3 +28,57 @@ require random.fs
  length rand-fill
  length ;
 
+: rand-bool { percent-true -- n }
+ 100 random
+ 0 percent-true between? ;
+
+: clone-string { addr length -- addr }
+ here length chars allot { new-addr }
+ addr new-addr length cmove
+ new-addr ;
+
+\ -------------------------------------
+s" METHINKS IT IS LIKE A WEASEL"
+Constant GOAL-LENGTH
+
+here GOAL-LENGTH chars allot
+Constant GOAL-STRING
+
+GOAL-STRING GOAL-LENGTH cmove
+
+25 Constant MUTATE-PERCENT
+
+
+: make-attempt ( -- addr )
+ GOAL-LENGTH rand-string drop ;
+ 
+: .attempt ( addr -- )
+ GOAL-LENGTH type ;
+
+: clone-attempt ( addr -- new-addr )
+ GOAL-LENGTH clone-string ;
+
+: mutate? ( -- yes-or-no )
+ MUTATE-PERCENT rand-bool ;
+
+: mutate-char ( addr -- )
+ mutate? IF
+  rand-char swap c!
+ else
+  drop
+ endif ;
+ 
+: mutate-attempt { addr -- addr }
+ GOAL-LENGTH 0 u+do
+   addr i + mutate-char
+ loop addr ;
+
+: .x-attempts { n -- }
+ make-attempt
+ n 0 u+do
+  dup
+  cr
+  clone-attempt
+  mutate-attempt
+  .attempt
+ loop ; 
