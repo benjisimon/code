21f38f771fc97b8b66db2fbf8ddbf8ba8e781d01
ben <ben@ideas2executables.com>
2015-03-16 11:30:05 +0000

    experimented with writeonly environment

---
+++ writeonly/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>WriteOnly</title>
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+  <link rel="Stylesheet" href="layout.css" type="text/css"/>
+ </head>
+ <body>
+  <div class='feedback'></div>
+  <div class='writeonly' tabindex="1">_</div>
+  <div class='footer'>
+   By <a href='http://blogbyben.com'>Ben</a>
+  </div>
+  <script src="writeonly.js"></script>
+ </body>
+</html> 
