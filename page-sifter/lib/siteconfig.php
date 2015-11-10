95dce3c1d516af9d058c929a0f63e18df8873577
Ben Simon <ben@ideas2executables.com>
2015-11-10 05:47:32 -0500

    Implemented a quick PHP script to help sift through awful HTML and keep the Good Stuff for a website migration project.

---
+++ page-sifter/lib/siteconfig.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * A PHP file for handling our top level configuration
+ */
+
+require_once(__DIR__ . '/../shared/lib/utils.php');
+require_once(__DIR__ . '/../shared/lib/simple_html_dom.php');
+require_once(__DIR__ . '/../.conf.php');
+require_once(__DIR__ . '/sift.php');
+
+
+?>
