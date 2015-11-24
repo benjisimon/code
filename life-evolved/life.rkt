2ff5f2ee67311d1af3de9d294abe717abe0be996
Ben Simon <ben@ideas2executables.com>
2015-11-24 13:49:16 +0000

    And we have life! It's not efficient, but it's implmented. Whoo!

--- life-evolved/life.rkt
+++ life-evolved/life.rkt
@@ -4,13 +4,41 @@
 ;; A very primitive implementation of Conway's Game of Life
 ;;
 
+(define (inc x)
+  (+ 1 x))
+
+(define (dec x)
+  (- x 1))
+
+(define (bounds board)
+  (values (vector-length (vector-ref board 0))
+          (vector-length board)))
+
+
 (define (cell-set! board x y value)
-  (vector-set! (vector-ref board y)
-               x
-               value))
+  (let-values ([(w h) (bounds board)])
+    (let ([x (modulo x w)]
+          [y (modulo y h)])
+    (vector-set! (vector-ref board y)
+                 x
+                 value))))
 
 (define (cell-ref board x y)
-  (vector-ref (vector-ref board y) x))
+  (let-values ([(w h) (bounds board)])
+    (let ([x (modulo x w)]
+          [y (modulo y h)])
+      (vector-ref (vector-ref board y) x))))
+
+(define (neighbors board x y)
+  (let ([ref (lambda (x y) (cell-ref board x y))])
+    (list (ref (dec x) y)
+          (ref (dec x) (dec y))
+          (ref x       (dec y))
+          (ref (inc x) (inc y))
+          (ref (inc x) y)
+          (ref (inc x) (dec y))
+          (ref x       (dec y))
+          (ref (dec x) (dec y)))))
 
 (define (print board)
   (for ([row board])
@@ -30,11 +58,47 @@
           (cell-set! board x y #t))))
     board))
 
+(define (survive? board x y)
+  (let* ([alive? (cell-ref board x y)]
+         [friends (neighbors board x y)]
+         [num-living-friends (length (filter identity friends))])
+    (cond ([and alive? (< num-living-friends 2)] #f)
+          ([and alive? (or (= num-living-friends 2)
+                           (= num-living-friends 3))] #t)
+          ([and alive? (> num-living-friends 3)] #f)
+          ([and (not alive?) (= num-living-friends 3)] #t)
+          (else #f))))
+    
+    
+
+(define (tick board)
+  (let-values ([(w h) (bounds board)])
+    (let ([next (bang w h '())])
+      (for ([row board]
+            [y (in-naturals)])
+        (for ([cell row]
+              [x (in-naturals)])
+          (cell-set! next x y (survive? board x y))))
+      next)))
+
+(define (play generations board)
+  (if (= generations 0) board
+      (play (dec generations) (tick board))))
+
+
 (define b
-  (bang 20 20
+  (bang 8 8
         '((_ X _ X)
           (_ _ X X)
           (X _ _ _))))
+
+
+(print (play 10 b))
+
+(print b)
+(print (tick b))
+(print (tick (tick b)))
+(print (tick (tick (tick b))))
 
 (define c
   (bang 3 3
