6d3a62fd8674a12e1ef29d54b8d50b9f09af205c
Ben Simon <ben@ideas2executables.com>
2016-01-08 04:23:09 +0000

    Yet another pass at spot-it. Not working yet, but much closer.

--- programming-praxis/spot-it.scm
+++ programming-praxis/spot-it.scm
@@ -12,59 +12,78 @@
 (define (dec x)
   (- x 1))
 
-(define (init-deck size)
-  (for/vector ([i size])
-    (list)))
+(define (make-sym-generator)
+  (let ([x 0])
+    (lambda ()
+      (set! x (inc x))
+      x)))
 
 (define (card-match? c1 c2)
   (cond ([null? c1] #f)
         ([member (car c1) c2] #t)
         (else (card-match? (cdr c1) c2))))
 
-(define (find-matches c deck)
-  (filter (lambda (x)
-            (card-match? c x)) (vector->list deck)))
-
-(define (random-symbol deck)
-  (random (vector-length deck)))
-
-(define (random-card-position deck)
-  (random (vector-length deck)))
-
-(define (can-add? sym card deck)
-  (cond ([member sym card] #t)
-        (else
-         (let ([suspects (find-matches card deck)])
-           (let loop ([suspects suspects])
-             (cond ([null? suspects] #t)
-                   ([member sym (car suspects)] #f)
-                   (else
-                    (loop (cdr suspects)))))))))
-
-(define (add-sym card sym)
-  (if (member sym card)
-      card
-      (cons sym card)))
-      
-(define (tick! deck)
-  (let* ([p1 (random-card-position deck)]
-         [p2 (random-card-position deck)]
-         [c1 (vector-ref deck p1)]
-         [c2 (vector-ref deck p2)])
-    (cond ([card-match? c1 c2] deck)
+(define (can-add-to-bucket? card bucket)
+  (let loop ([i 0])
+    (cond ([>= i (vector-length bucket)] #f)
+          ([eq? (vector-ref bucket i) #f] #t)
+          ([card-match? (vector-ref bucket i) card] #f)
           (else
-           (let ([sym (random-symbol deck)])
-            (when (and (can-add? sym c1 deck)
-                       (can-add? sym c2 deck))
-              (vector-set! deck p1 (add-sym c1 sym))
-              (vector-set! deck p2 (add-sym c2 sym)))
-            deck)))))
+           (loop (inc i))))))
 
-(define d (init-deck 48))
-(tick! d)
-(for ([i 10000])
-  (tick! d))
-d
+(define (make-buckets n)
+  (for/vector ([i n])
+     (make-vector n #f)))
 
-(length (find-matches (vector-ref d 0) d))
-  
+(define (make-deck n)
+  (if (exact? (sqrt n))
+      (make-list n '())
+      (error "Deck size must a perfect square")))
+
+(define (bucket-add! bucket card)
+  (let loop ([i 0])
+    (cond ([>= i (vector-length bucket)]
+           (error (format "Whoops, no space to store this card: ~s: ~s" card bucket)))
+          ([eq? (vector-ref bucket i) #f]
+           (vector-set! bucket i card))
+          (else
+           (loop (inc i))))))
+
+(define (add-to-bucket! card buckets)
+  (let loop ([i 0])
+    (cond ([>= i (vector-length buckets)]
+           (error (format "Can't find a valid bucket for ~s: ~s" card buckets)))
+          ([can-add-to-bucket? card (vector-ref buckets i)]
+           (bucket-add! (vector-ref buckets i) card))
+          (else (loop (inc i))))))
+
+(define (bucket-matchify bucket sym-gen)
+  (let ([sym (sym-gen)])
+    (for/list ([c bucket])
+      (cons sym c))))
+
+(define (tick cards num-buckets sym-generator)
+  (let ([buckets (make-buckets num-buckets)])
+    (let loop ([cards cards])
+      (cond ([null? cards]
+             #t)
+            (else
+             (let ([card (car cards)])
+               (add-to-bucket! card buckets)
+               (loop (cdr cards))))))
+    (let loop ([i 0] [cards '()])
+      (cond ([>= i (vector-length buckets)] cards)
+            (else
+             (loop (inc i)
+                   (append cards
+                           (bucket-matchify
+                            (vector-ref buckets i) sym-generator))))))))
+
+
+(define (go size)
+  (let ([sym-gen (make-sym-generator)])
+    (let loop ([generation 0] [cards (make-deck size)])
+      (display (format "deck[~s]: ~s\n" generation cards))
+      (loop (inc generation) (tick cards (sqrt size) sym-gen)))))
+
+(go 9)
