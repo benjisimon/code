8141477d743c41b43a9a04395dccf4dd0ead05ed
Ben Simon <ben@ideas2executables.com>
2016-06-09 14:02:09 -0400

    Experimenting with a url shortening bookmark

---
+++ bookmarklets/quick-url.js
@@ -0,0 +1,16 @@
+/*
+ * This is a javascript file used for generating a bookmarklet to a short but sweet URL.
+ */
+(function() {
+  function c() {
+    var dict = ['A','B','C','D','E','F','G','H','J','K','L','M','N',
+                'P','Q','R','S','T','U','W','X','Y','Z',
+                2,3,4,5,6,7,8,9];
+    return dict[Math.floor(Math.random() * dict.length)];
+  }
+  window.location =
+    ('http://u.ideas2executables.com/admin.py?hash=True&main=True&action=EDIT&' +
+     'url='+encodeURIComponent(location.href) + '&' +
+     'path=' + c() + c() + c() + c());
+}())
+    
