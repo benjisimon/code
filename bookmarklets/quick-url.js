88904ef888faeb518c0d8de8ec1ae3ae52bfcda6
Ben Simon <ben@ideas2executables.com>
2016-06-10 11:27:06 -0400

    Open our shrink-url UI in a new tab

--- bookmarklets/quick-url.js
+++ bookmarklets/quick-url.js
@@ -8,9 +8,8 @@
                 2,3,4,5,6,7,8,9];
     return dict[Math.floor(Math.random() * dict.length)];
   }
-  window.location =
-    ('http://u.ideas2executables.com/admin.py?hash=True&main=True&action=EDIT&' +
-     'url='+encodeURIComponent(location.href) + '&' +
-     'path=' + c() + c() + c());
+  window.open('http://u.ideas2executables.com/admin.py?hash=True&main=True&action=EDIT&' +
+              'url='+encodeURIComponent(location.href) + '&' +
+              'path=' + c() + c() + c(), '_blank');
 }())
     
