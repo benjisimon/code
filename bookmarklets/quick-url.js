b406a930b7452a98e9fd4961d28832bc2cca6eae
Ben Simon <ben@ideas2executables.com>
2016-06-23 13:49:01 -0400

    who knew that 6 & G are ambigous? In my handwriting, they are.

--- bookmarklets/quick-url.js
+++ bookmarklets/quick-url.js
@@ -3,9 +3,9 @@
  */
 (function() {
   function c() {
-    var dict = ['A','B','C','D','E','F','G','H','J','K','L','M','N',
+    var dict = ['A','B','C','D','E','F',,'H','J','K','L','M','N',
                 'P','Q','R','S','T','U','W','X','Y','Z',
-                2,3,4,5,6,7,8,9];
+                2,3,4,5,7,8,9];
     return dict[Math.floor(Math.random() * dict.length)];
   }
   window.open('http://u.ideas2executables.com/admin.py?hash=True&main=True&action=EDIT&' +
