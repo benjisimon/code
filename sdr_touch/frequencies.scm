1b7e2f11159fcf4ff7d6144a3262368279d8db12
Ben Simon <benjisimon@gmail.com>
2017-11-17 09:26:40 -0500

    Experiment with tracking SDR frequencies in scheme
    Also have a script to generate the master list of frequencie

---
+++ sdr_touch/frequencies.scm
@@ -0,0 +1,18 @@
+;;
+;; Our database of interesting frequencies
+;;
+
+(("Arl,VA"
+  (166.9 "DC Police?")
+  (166.7 "Dc Police?"))
+
+ ("OnABoat"
+  (462 "FRS Low")
+  (467 "FRS High")
+  (457.5 "Marine1")
+  (467.5 "Marine2")
+  (467.7 "Marine3"))
+
+ )
+
+ 
