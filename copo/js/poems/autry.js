f8fdbd9b83f2a5f23a78a260e9479895a5f7caec
Ben Simon <ben@ideas2executables.com>
2018-02-06 08:50:41 -0500

    Experimented with bpm hacking

--- copo/js/poems/autry.js
+++ copo/js/poems/autry.js
@@ -11,11 +11,13 @@ Audio.setup(function(ctx) {
     { type: 'frequency', value: Note.A, at: 0, duration: ctx.width }
   ];
 
-  for(var i = 0; i < (ctx.width * 4); i++) {
+  for(var i = 0; i < (ctx.width * 4); i += 3) {
     right.push({ type: 'gain', value: .6, at: i, duration: .25 });
   }
 
+  ctx.bpm     = ctx.bpm > 300 ? 120 : (ctx.bpm + 50);
   ctx.players = [ left, right ];
   
+  console.log(ctx);
   return ctx;
 });
