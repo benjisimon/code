680c595574276c99ca8c7727d5d97df874a99f73
Ben Simon <ben@ideas2executables.com>
2018-02-26 13:19:17 -0500

    Rough musical grid put in place. Whoo.

--- dragiano/index.php
+++ dragiano/index.php
@@ -15,6 +15,7 @@ $bpm = 180;
 			  src="https://code.jquery.com/jquery-3.3.1.min.js"
 			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 			  crossorigin="anonymous"></script>
+    <script src="shared/js/concrete.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/layout.css?ver=<?= filemtime(__DIR__ . "/css/layout.css")?>"/>
   </head>
 
@@ -24,8 +25,8 @@ $bpm = 180;
       <input type="button" class="go" value="Go"/>
     </div>
 
-    <canvas class="surface">
-    </canvas>
+    <div class="surface">
+    </div>
 
     <script src="js/ui.js?ver=<?= filemtime(__DIR__ . "/js/ui.js")?>"></script>    
   </body>
--- dragiano/js/ui.js
+++ dragiano/js/ui.js
@@ -4,22 +4,68 @@
 
 
 var Ui = {
+  config: function() {
+    return { surface: { width: $('body').width(),
+                        height:  $(document).innerHeight() - $('.controls').innerHeight() - 10 },
+             beats: 12l };
+  },
+
+  makeGrid: function(viewport) {
+    var config = Ui.config();
+    var grid = new Concrete.Layer();    
+    var gridGap =  config.surface.width / config.beats;
+
+    grid.setSize(config.surface.width, config.surface.height);
+    viewport.add(grid);
+
+    for(var i = 1; i < config.beats; i++) {
+      grid.scene.context.beginPath();
+      grid.scene.context.strokeStyle = '#850000';
+      grid.scene.context.lineWidth = 4;
+      grid.scene.context.moveTo(gridGap * i, 0);
+      grid.scene.context.lineTo(gridGap * i, config.surface.height);
+      grid.scene.context.stroke();
+      viewport.render();
+    }
+
+    window.xx = grid;
+    return grid;
+  },
+
   pack: function() {
-    $('canvas').attr('width', 10).attr('width', $('body').width());
-    $('canvas').attr('height', 10).attr('height', $(document).innerHeight() - $('.controls').innerHeight() - 10);
+    Ui.viewport = new Concrete.Viewport({
+      width: Ui.config().surface.width,
+      height: Ui.config().surface.height,
+      container: $('.surface').get(0)
+    });
+
+    Ui.makeGrid(Ui.viewport);
 
     return true;
-  }
+  },
+
+  start: function() {
+    
+    },
+
+  stop: function() {
+
+    }
 
 };
 
 $(document).ready(function() {
   Ui.pack();
-
 });
 
 $(window).on('resize', Ui.pack);
 
 $(document).on('click', '.go', function() {
-  return false;
+  if($(this).val() == 'Go') {
+    Ui.start();
+    $(this).val("Stop");
+  } else {
+    Ui.stop();
+    $(this).val("Go");
+  }
 });
