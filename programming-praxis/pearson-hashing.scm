406a19518e3e00ea219aa73c98bf3524c0d18ad7
Ben Simon <ben@ideas2executables.com>
2018-06-01 11:01:56 -0400

    Added a quick function to check the pearson caching for distribution

--- programming-praxis/pearson-hashing.scm
+++ programming-praxis/pearson-hashing.scm
@@ -49,4 +49,22 @@
           (loop (vector-ref (*T*) (%256 (+ hash (car chars))))
                 (cdr chars))))))
 
+(define (pearson-check)
+  (let ((results  (make-vector 256 0)))
+  (call-with-input-file "../../../../google-10000-english/google-10000-english.txt"
+    (lambda (p)
+      (let loop ((line (read-line p)))
+        (cond ((eof-object? line)
+               (for-each (lambda (i)
+                           (let ((count (vector-ref results i)))
+                             (for-each (lambda (j)
+                                         (display "."))
+                                       (range 0 count))
+                             (newline)))
+                         (range 0 256)))
+              (else
+               (let ((h (pearson-hash line)))
+                 (vector-set! results h (+ (vector-ref results h) 1))
+                 (loop (read-line p))))))))))
+                     
         
