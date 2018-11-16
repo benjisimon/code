bf63fbba8a101a99d740dcd80f92529e04de2ba0
Ben Simon <ben@ideas2executables.com>
2018-11-16 10:08:02 -0500

    Setup 'real' civil war example

--- programming-praxis/civil-war-cipher.scm
+++ programming-praxis/civil-war-cipher.scm
@@ -2,7 +2,28 @@
 ;; Implement a cipher used in the US Civil War
 ;; http://www.mathaware.org/mam/06/Sauerberg_route-essay.html
 ;; https://programmingpraxis.com/2012/03/06/union-route-cipher/
+;; http://www.civilwarsignals.org/pages/crypto/crypto.html
 ;;
+
+(define (take n src)
+  (cond ((= n 0) '())
+        ((null? src) '())
+        (else
+         (cons (car src)
+               (take (- n 1)
+                     (cdr src))))))
+
+(define (drop n src)
+  (cond ((= n 0) src)
+        ((null? src) '())
+        (else
+         (drop (- n 1) (cdr src)))))
+
+(define (group-into size src)
+  (let ((g (take size src)))
+    (cond ((null? g) '())
+          (else
+           (cons g (group-into size (drop size src)))))))
 
 (define (jumble src route)
   (if (null? route) 
@@ -33,62 +54,116 @@
         (else 
          (decode word (cdr dictionary)))))
 
-(define (encrypt src seq dict)
-  (jumble (map (lambda (w) 
-                 (encode w dict)) 
-               src) 
-          seq))
+(define (encrypt src routes dict)
+  (let ((found (assoc (car src) routes)))
+    (if found
+        (let* ((route-name (car found))
+               (seq (cdr found))
+               (lhs (take (length seq) src))
+               (rhs (drop (length seq) src)))
+          (append
+           (list route-name)
+           (jumble (map (lambda (w) 
+                          (encode w dict)) 
+                        lhs)
+                   seq)
+           (encrypt rhs routes dict)))
+        '())))
+           
+          
+       
 
-(define (decrypt src seq dict)
+ (let ((seq (assoc (car src) routes) =>
+             (lambda (
+
+(define (decrypt src routes dict)
   (unjumble (map (lambda (w) 
                  (decode w dict)) 
                src) 
           seq))
 
-(define (take n src)
-  (cond ((= n 0) '())
-        ((null? src) '())
-        (else
-         (cons (car src)
-               (take (- n 1)
-                     (cdr src))))))
+;;
+;; Routes, dictionary and message from http://www.civilwarsignals.org/pages/crypto/crypto.html
+;;
+(define routes
+  '((enemy . (46 24 00 23 47 69
+              47 25 01 22 46 68
+              48 26 02 21 45 67
+              49 27 03 20 44 66
+              50 28 04 19 43 65
+              51 29 05 18 42 64
+              52 30 06 17 41 63
+              53 31 07 16 40 62
+              54 32 08 15 39 61
+              55 33 09 14 38 60
+              56 34 10 13 37 59
+              57 35 11 12 36 58))
 
-(define (drop n src)
-  (cond ((= n 0) src)
-        ((null? src) '())
-        (else
-         (drop (- n 1) (cdr src)))))
+    (stanton . (07 20 34 47 33 19
+                06 21 35 46 32 18
+                08 05 36 45 31 17
+                09 22 04 44 30 16
+                10 23 37 03 29 15
+                11 24 38 43 02 14
+                12 25 39 42 28 01
+                13 26 40 41 27 00))
 
-(define (group-into size src)
-  (let ((g (take size src)))
-    (cond ((null? g) '())
-          (else
-           (cons g (group-into size (drop size src)))))))
+    (mcdowell . (07 20 34 47 33 19
+                 06 21 35 46 32 18
+                 08 05 36 45 31 17
+                 09 22 04 44 30 16
+                 10 23 37 03 29 15
+                 11 24 38 43 02 14
+                 12 25 39 42 28 01
+                 13 26 40 41 27 00))))
 
+(define dict '((burnside . burton)
+               (enemy . wiley)
+               (cavalry . roanoke)
+               (our . over)
+               (cavalry . relay)
+               (rebels . snow)
+               (tennessee . godwin)
+               (rosecrans . benet)
+               (seven-pm . julia)))
 
-(define route '(3 6 2
-                4 7 1
-                5 8 0))
+(define msg 
+  '(enemy 
+    __ __ __ boy greatly __ __
+    for burnside the enemy are preparing
+    pontoons and increasing numbers on our
+    front if they cross between us
+    you will go up and probably
+    we too x you ought to
+    move in the direction at least
+    as far as kingston which should
+    be strongly fortified and your spare
+    stores go into it without delay
+    you ought to be free to
+    not surely some __ __ __
+    
+    stanton
+    fortune __ __ __ the time
+    oppose a crossing and with your
+    cavalry to keep open complete and
+    rapid communications between us so that
+    we can move combinedly on him
+    let me hear from you if
+    possible at once no news from
+    speed this more __ __ __
+    
+    mcdowell
+    must __ __ __ mind horses
+    you in ten days our cavalry
+    drove the rebel across the tennessee 
+    at lambs ferry with loss to
+    them of two thousand killed wounded
+    prisoners and deserters also five pieces
+    of artillery yours rosecrans answer quick seven-pm
+    men truly gorden __ __ __))
 
-(define msg '(meet at first
-              light next to
-             the large pine))
-
-(define dict '((meet . cow)
-               (next . lightbulb)
-               (pine . the)))
-
-
-(encode 'meet dict)
-(encode 'friday dict)
-(decode (encode 'meet dict) dict)
-(decode (encode 'friday dict) dict)
-
-(unjumble (jumble msg route) route)
-
-(group-into 3 (encrypt msg route dict))
-(group-into 3 (decrypt (encrypt msg route dict) route dict))
-
+(encrypt msg routes dict)
+    
 
                   
                 
