c7be4a9ce7ef4c60902428d8babcaff4214d930f
Ben Simon <ben@ideas2executables.com>
2019-01-02 01:41:13 -0500

    Support future weather dates by averaging the last 5 years

--- singular-weather-service/lib/weather.php
+++ singular-weather-service/lib/weather.php
@@ -10,12 +10,37 @@ function forecast($loc, $time, $attr) {
   $ttl = $time < time() ? true : (60*60*6);
 
   if($time > (time() + (60*60*24*10))) {
-    die("Future time asked for!");
+    $ref_times = historic_times($time, 5);
+    $result    = false;
+    foreach($ref_times as $t) {
+      $f  = forecast($loc, $t, $attr);
+      if(is_numeric($f)) {
+        $result = $result === false ? $f : (($result + $f) / 2);
+      } else {
+        $result = ($result == false ? $f : ("$result\n\n$f"));
+      }
+    }
+    return $result;
   } else {
     $response = curl_get($url, ['exclude' => 'hourly,currently,flags'], ['ttl' => $ttl]);
     $data = $response['daily']['data'][0];
     return $attr === 'all' ? json_encode($data, JSON_PRETTY_PRINT) : $data[$attr];
   }
+}
+
+function historic_times($future, $count) {
+  $year      = date('Y');
+  $collected = [];
+
+  while(count($collected) < $count) {
+    $t   = strtotime(date("{$year}-m-d H:i", $future));
+    $year--;
+    if($t > time()) {
+      continue;
+    }
+    $collected[] = $t;
+  }
+  return $collected;
 }
 
 ?>
