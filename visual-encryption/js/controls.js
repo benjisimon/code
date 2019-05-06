37986219a9a2bd73a57f5bb5de06c985d6d9b1a8
Ben Simon <ben@ideas2executables.com>
2019-05-06 08:29:42 -0400

    Started experimenting with encryption funcitionality

--- visual-encryption/js/controls.js
+++ visual-encryption/js/controls.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   });
 
+  $(document).on('click', '.encrypt', function() {
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(imageData.data);
+    for(var i = 0; i < imageData.data.length; i += 4) {
+      var r = imageData.data[i];
+      var g = imageData.data[i+1];
+      var b = imageData.data[i+2];
+      var a = imageData.data[i+3];
+
+      if(a == 0) {
+        continue;
+      }
+
+      if(a == 255 & (r == g == b == 0)) {
+        var x  = i % canvas.width;
+        var y  = Math.floor(i / 4 / canvas.width);
+        console.log(x, y);
+      }
+
+    }
+  });
+
 });
