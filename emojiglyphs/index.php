862b3aa57555e100d3c067eee7741d8af7e76b20
Ben Simon <ben@ideas2executables.com>
2020-02-12 15:43:08 -0500

    more progress with emoji selection

--- emojiglyphs/index.php
+++ emojiglyphs/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * A PHP file for generating emojigraphs
+ * A PHP file for generating emojiglyphs
  */
 require_once(__DIR__ . '/lib/utils.php');
 
@@ -8,7 +8,7 @@ require_once(__DIR__ . '/lib/utils.php');
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Emojigraphs</title>
+    <title>Emojiglypths</title>
     
     <script
       src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -18,16 +18,19 @@ require_once(__DIR__ . '/lib/utils.php');
       src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
       integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
       crossorigin="anonymous"></script>
+    <link rel="Stylesheet" type="text/css" href="css/layout.css?ver=<?= filemtime(__DIR__ . "/css/layout.css")?>"/>
   </head>
   
   <body>
-    <input name="q"/>
-    
-    <div class="results">
-
+    <div class="page">
+      <input name="q" placeholder="Search for an emoji" />
+      
+      <div class="board">
+      </div>
     </div>
 
-    <script src="ui.js?ver=<?= filemtime(__DIR__ . '/ui.js')?>"></script>
+    <script src="js/jquery.ui.autocomplete.html.js?ver=<?= filemtime(__DIR__ . '/js/jquery.ui.autocomplete.html.js')?>"></script>
+    <script src="js/ui.js?ver=<?= filemtime(__DIR__ . '/js/ui.js')?>"></script>
       
   </body>
 </html>
--- emojigraphs/ui.js
+++
@@ -1,9 +0,0 @@
-/*
- * This is a javascript file used for implementing our ui
- */
-
-$(document).ready(function() {
-  $('input[name=q]').autocomplete({
-    source: "lookup.php"
-  });
-});
