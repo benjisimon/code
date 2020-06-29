cb5f78d79457f2ed63c0bd02eea42bd78a864c2e
Ben Simon <benjisimon@gmail.com>
2020-06-29 20:53:32 +0000

    impl different approaches to growth

--- programming-praxis/growth.scm
+++ programming-praxis/growth.scm
@@ -6,18 +6,24 @@
 (define ($ amt)
   (inexact amt))
 
-(define (grow balance rate)
+
+(define (grow/1 balance rate)
   (let ((interest (* balance (/ rate 100))))
     ($ (+ balance interest))))
 
-(define (grow balance rate frequency)
-  (let ((r (/ (/ rate frequency) 100)))
-    (let loop ((balance balance) (frequency frequency))
+(define (grow/2 balance rate frequency)
+  (let ((r (if (or (= 0 rate) (= 0 frequency))  0
+               (/ (/ rate frequency) 100))))
+    (let loop ((balance balance) 
+               (frequency frequency))
       (cond ((= frequency 0) ($ balance))
             (else
-             (let ((interest (* balance r)))
-               (loop 
-                ($ (+ balance interest))
-                (- frequency 1))))))))
+             (let* ((interest (* balance r)))
+               (loop ($ (+ balance interest))
+                     (- frequency 1))))))))
 
+
+
+(define (grow/3 balance rate)
+  ($ (* balance (exp (/ rate 100)))))
 
