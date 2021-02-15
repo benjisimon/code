03d85a0ebd60ed4d00ba0bfeb1aba853bcdb7b57
Ben Simon <ben@ideas2executables.com>
2021-02-15 12:23:34 -0500

    basic websocket example

--- mqboard/index.php
+++ mqboard/index.php
@@ -8,13 +8,21 @@
  */
 
 $env = [
-  'wss_url' => 'wss://b-fb1df8e2-cf9a-44f6-8071-40ecdc7a772c-1.mq.us-east-1.amazonaws.com:61619/mqboard/1'
+  'host'     => 'b-fb1df8e2-cf9a-44f6-8071-40ecdc7a772c-1.mq.us-east-1.amazonaws.com',
+  'port'     => 61619,
+  'path'     => '/',
+  'username' => 'mqboard',
+  'password' => 'c50504b9789548f371836c112d6aba62',
+  'clientId' => 'MqBoardSubscriber',
+  'topic'    => 'mqboard/1'
 ];
 
 ?>
 <html>
   <title>MqBoard</title>
   <link rel="Stylesheet" type="text/css" href="layout.css?ver=<?= md5_file(__DIR__ . '/layout.css') ?>"/>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
   <script>
    var Env = <?= json_encode($env); ?>;
   </script>
--- mqboard/ui.js
+++ mqboard/ui.js
@@ -2,4 +2,24 @@
  * A JS file for powering our ui
  */
 
+var client = new Paho.MQTT.Client(Env.host, Env.port, Env.path, Env.clientId);
+
+client.onMessageArrived = function(m) {
+  $('.message').html(m.payloadString);
+};
+
+client.connect({
+  userName: Env.username,
+  password: Env.password,
+  useSSL: true,
+  mqttVersion: 3,
+  onFailure: function(e) {
+    console.log("FAIL!");
+  },
+  onSuccess: function() {
+    console.log("WHoo!");
+    client.subscribe(Env.topic);
+  }
+});
+
 
