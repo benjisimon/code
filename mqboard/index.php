0d89035670c5391ee2a9f49d3b7476ed1766c484
Ben Simon <ben@ideas2executables.com>
2021-02-15 19:10:12 +0000

    fixed ipad friendly tag to go full screen

--- mqboard/index.php
+++ mqboard/index.php
@@ -19,20 +19,20 @@ $env = [
 
 ?>
 <html>
-  <title>MqBoard</title>
-  <link rel="Stylesheet" type="text/css" href="layout.css?ver=<?= md5_file(__DIR__ . '/layout.css') ?>"/>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
-  <script>
-   var Env = <?= json_encode($env); ?>;
-  </script>
-  <meta name=”apple-mobile-web-app-capable” content=”yes”>
+  <head>
+    <title>MqBoard</title>
+    <link rel="Stylesheet" type="text/css" href="layout.css?ver=<?= md5_file(__DIR__ . '/layout.css') ?>"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
+    <script>
+     var Env = <?= json_encode($env); ?>;
+    </script>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+  </head>
   <body>
     <div class="message">
-      Hello World
     </div>
     <div class="seq">
-      3
     </div>
   </body>
   <script src="ui.js?ver=<?= md5_file(__DIR__ . '/ui.js')?>"></script>
