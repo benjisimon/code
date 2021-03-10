3be0cbec20cb87661c48938f6648d8aa253b8075
Ben Simon <ben@ideas2executables.com>
2021-03-09 19:18:23 -0500

    styled and fixed timezone

--- on-this-day-pics/index.php
+++ on-this-day-pics/index.php
@@ -3,9 +3,12 @@
  * A PHP file for providing a simple service: what photos were taken on this
  * day in your album?
  */
+
+
 require_once(__DIR__ . '/lib/siteconfig.php');
 
-$num_years = 10;
+
+$num_years = 11;
 $is_authenticated = handle_oauth2();
 $today = ['month' => date('m', time()),
           'day'   => date('d', time())];
@@ -14,29 +17,48 @@ $today = ['month' => date('m', time()),
 <html>
   <head>
     <title>On This Day</title>
+    <link rel="Stylesheet" type="text/css"
+          href="<?= app_url('layout.css', ['version' => md5_file(__DIR__ . '/layout.css')])?>"
   </head>
 
   <body>
-    <? if($is_authenticated) { ?>
-      <? foreach(range(date('Y') - $num_years, date('Y')) as $year) { ?>
-        <h2><?= $year ?></h2>
+    <div class="main">
+      <h1>On This Day | Pics</h1>
+      
+      <? if($is_authenticated) { ?>
+        <? foreach(range(date('Y') - $num_years, date('Y')) as $year) { ?>
+          <h2><?= date('F, jS') ?> <?= $year ?></h2>
 
-        <div class="photos">
-          <? $photos = photo_date_search(['year' => $year] + $today); ?>
-          <? foreach($photos as $item) { ?>
-            <a href="<?= $item->getProductUrl() ?>">
-              <img src="<?= $item->getBaseUrl() ?>=w300-h300"/>
-            </a>
-          <? } ?>
-        </div>
-      <? }  ?>
+          <div class="photos">
+            <? $photos = photo_date_search(['year' => $year] + $today); ?>
+            <? foreach($photos as $item) { ?>
+              <div class="photo">
+                <a href="<?= $item->getBaseUrl() ?>=w2000-h2000">
+                  <img src="<?= $item->getBaseUrl() ?>=w150-h150"/>
+                </a>
+                <div class="controls">
+                  <a class="al" href="<?= $item->getProductUrl() ?>">Info</a>
+                  <a class="ar" href="<?= $item->getBaseUrl() ?>=d">Download</a>
+                </div>
+              </div>
+            <? } ?>
+          </div>
+        <? }  ?>
 
-      <p>
-        <a href="<?= app_url('', ['code' => 'clear'])?>">Log Out</a></a>
-      </p>
-    <? } else { ?>
-      <a href="<?= app_url('', ['code' => 'create'])?>">Authenticate</a></a>
-    <? } ?>
+        <p>
+          <a href="<?= app_url('', ['code' => 'clear'])?>">Log Out</a></a>
+        </p>
+      <? } else { ?>
+        <p>
+          Welcome. To continue
+          <a href="<?= app_url('', ['code' => 'create'])?>">grant access.</a>
+        </p>
+      <? } ?>
+    </div>
+
+    <div class="footer">
+      Built By <a href="https://blogbyben.com/">Ben</a>
+    </div>
   </body>
 </html>
 
--- on-this-day-pics/lib/siteconfig.php
+++ on-this-day-pics/lib/siteconfig.php
@@ -3,6 +3,7 @@
  * A PHP file for loading our site's configuration
  */
 
+date_default_timezone_set('America/New_York');
 define('CREDENTIALS_JSON', '/var/www/private/on-this-day-pics.credentials.json');
 
 require_once(__DIR__ . '/vendor/autoload.php');
