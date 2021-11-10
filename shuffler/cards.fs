4024c68f99ac30df210f89a60f7659c98232ee29
Ben Simon <ben@ideas2executables.com>
2021-11-09 20:29:23 -0500

    gah - there's not an Ace and a 1 in a deck of cards.

--- shuffler/cards.fs
+++ shuffler/cards.fs
@@ -17,9 +17,9 @@ create suit-symbols char H c, char C c,  char S c, char D c,
 : suit>sym ( suit -- c )
     suit-symbols + c@ ;
 
-create rank-symbols char A c, char 1 c, char 2 c, char 3 c,
+create rank-symbols char A c, char 2 c, char 3 c,
 char 4 c, char 5 c, char 6 c, char 7 c, char 8 c,
-char 9 c, char X c, char J c, char Q c, char K c, char A c,
+char 9 c, char X c, char J c, char Q c, char K c,
 
 
 : rank>sym ( rank -- c )
