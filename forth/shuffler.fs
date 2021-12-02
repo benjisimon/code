46dbf12e1594add879b5ce12d2775eebf61d063d
Ben Simon <ben@ideas2executables.com>
2021-12-01 20:41:39 -0500

    added a module. Why not

--- forth/shuffler.fs
+++ forth/shuffler.fs
@@ -18,13 +18,15 @@ require tests/coins.fs
 require tests/decks.fs
 require tests/shuffle.fs 
 
+module
+
 cr run-all cr cr
 new-deck shuffle .deck  cr
 new-deck 7*shuffle .deck cr cr
 
 variable #found
 
-: try-shuffle ( num-shuffles -- looks-to-be-shuffled? )
+:private try-shuffle ( num-shuffles -- looks-to-be-shuffled? )
     0 #found !
     100 0 +do
         dup new-deck swap *shuffle
@@ -32,7 +34,7 @@ variable #found
         7 = if #found @+1! then
     loop drop #found @ 0 > ;
 
-: try-shuffles ( -- )
+:private  try-shuffles ( -- )
     10 0 +do
         ." Shuffle x" i .
         i try-shuffle
@@ -41,3 +43,4 @@ variable #found
 
 try-shuffles
 
+publish
