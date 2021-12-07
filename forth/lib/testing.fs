0d52ba733b54d4e43ab30e3bee7f43c9050540df
Ben Simon <ben@ideas2executables.com>
2021-12-06 20:24:40 -0500

    simplified testing environment

--- forth/lib/testing.fs
+++ forth/lib/testing.fs
@@ -2,20 +2,11 @@
 
 module
 
-public-words
-
-struct
-    cell% field test-xt
-    cell% field test-name-length
-    cell% field test-name-addr
-    cell% field test-line
-end-struct test%
-
 private-words
 
 200 constant #max-tests
 create all-tests 
-#max-tests test% %size * allot
+#max-tests cells allot
 
 variable #tests
 0 #tests !
@@ -38,15 +29,10 @@ public-words
      #fails ? ." Failed";
 
 : nth-test ( n -- test-record )
-    test% %size * all-tests + ;
+    cells all-tests + ;
 
 : num-tests ( -- n )
     #tests @ ;
-
-:private test-name ( test-record -- c-addr u )
-    dup test-name-addr @
-    swap test-name-length @ ;
-
 
 :private current-test ( -- test-record )
     num-tests nth-test ;
@@ -54,26 +40,15 @@ public-words
 
 :private register-test ( xt -- )
     assert( num-tests #max-tests < )
-    current-test test-xt  !
-    sourcefilename current-test test-name-length !
-    current-test test-name-addr !
-    sourceline# current-test test-line !
+    current-test !
     #tests @+1! ;
-
-:private .test-outcome ( error-code -- )
-    dup =0 if ." OK" drop else  ."  ( " . ." )"  then ;
-
-:private .test-name ( test -- )
-    dup test-name type
-    ." :" 
-    test-line @ . ;
 
 : :test ( -- ) noname : latestxt register-test ;
 
 : run-all ( -- )
     reset-stats
     #tests @ 0  +do
-        i nth-test test-xt @ catch
+        i nth-test @ catch
         track-outcome
         clearstack
     loop
