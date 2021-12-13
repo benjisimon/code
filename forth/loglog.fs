24bbcf8e3d4467aabbec56c3fc92ae3ec96e5fdc
Ben Simon <ben@ideas2executables.com>
2021-12-13 08:06:51 -0500

    new code to experiment with loglog algorithm

---
+++ forth/loglog.fs
@@ -0,0 +1,17 @@
+\ Implement Loglog algorith from the
+\ programming praxis exercise
+
+require lib/modules.fs
+require lib/utils.fs
+require lib/bits.fs
+require lib/random.fs
+require lib/testing.fs
+require lib/hashing.fs
+
+require tests/modules.fs
+require tests/utils.fs
+require tests/bits.fs
+require tests/random.fs
+require tests/hashing.fs
+
+cr run-all cr cr
