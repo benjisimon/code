6477f4b9cac790f36c5679fac15ef77d65aed622
Ben Simon <ben@ideas2executables.com>
2022-01-03 18:47:36 -0500

    updated to put arrays in place

--- forth/loglog.fs
+++ forth/loglog.fs
@@ -10,8 +10,7 @@ require lib/files.fs
 require lib/random.fs
 require lib/testing.fs
 require lib/hashing.fs
-require lib/cons.fs
-require lib/lists.fs
+require lib/arrays.fs
 
 require tests/modules.fs
 require tests/utils.fs
@@ -19,18 +18,21 @@ require tests/bits.fs
 require tests/random.fs
 require tests/chars.fs
 require tests/hashing.fs
-require tests/cons.fs
-require tests/lists.fs
+require tests/arrays.fs
 
 cr run-all cr cr
 
 s" data/macbeth.txt" r/o open-file throw value macbeth-fd
 
+
 : next-word ( -- c-addr u | 0 )
     macbeth-fd read-word ;
 
-: max-hash ( -- n )
-    0
-    1000 0 +do
-        random-word hash max
-    loop ;
+: eof? ( x -- b )
+    0 = ;
+
+: collect-stats ( -- )
+    begin
+        next-word dup eof? not while
+    repeat ;
+
