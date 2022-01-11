81c26b454f129fcb497e256be25a01db7b8c2a62
Ben Simon <ben@ideas2executables.com>
2022-01-11 11:38:42 -0500

    Cleaner test

--- forth/tests/coins.fs
+++ forth/tests/coins.fs
@@ -10,23 +10,26 @@
     assert( flip coin? )
 ;
 
-create #heads 0 ,
-create #tails 0 ,
+0 value #heads
+0 value #tails
 
 : close-enough? ( x y -- b )
     - abs 30 < ;
 
 
 :test
-    assert( #heads @ 0 = )
-    assert( #tails @ 0 = )
+    randomize
+    0 to #heads
+    0 to #tails
+    assert( #heads 0 = )
+    assert( #tails 0 = )
 
     100 0 +do
         flip
-        heads? if #heads else #tails endif @+1!
+        heads? if 1 0 else 0 1 endif
+        #tails + to #tails
+        #heads + to #heads
     loop
 
-    assert( #heads @  #tails @ close-enough? )
-    
+    assert( #heads  #tails close-enough? )
 ;
-
