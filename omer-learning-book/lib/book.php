8175b7dcc514e171404a53187c51e0cff1403840
Ben Simon <ben@ideas2executables.com>
2022-06-01 00:14:24 -0400

    formatting improvements

--- omer-learning-book/lib/book.php
+++ omer-learning-book/lib/book.php
@@ -8,7 +8,12 @@ class Book extends FPDF {
 
   private $styles;
   private $current_style;
-
+  private $margins = [
+    'top' => 72,
+    'bottom' => 72,
+    'right' => 36,
+    'left' => 36
+  ];
 
 
   function __construct() {
@@ -25,11 +30,14 @@ class Book extends FPDF {
       'link' => ['aller', '', 12, [0, 0, 0]]
     ];
     $this->setStyle('default');
+    $this->AddPage();
+
+    $this->SetMargins($this->margins['left'], $this->margins['top'], $this->margins['right']);
+    $this->SetAutoPageBreak(true, $this->margins['bottom']);
   }
 
 
   public function addTitlePage() {
-    $this->AddPage();
     $this->SetY(72 * 2);
     $this->withStyle('book_title', function() {
       $this->Cell(0, 55, "49 Days to Greener and", 0, 2, 'C');
@@ -42,45 +50,69 @@ class Book extends FPDF {
       $this->Cell(0, 12, "Omer Learning 2022", 0, 2, 'C');
     });
 
-    $this->Ln(72 * 4);
+    $this->Ln(72 * 3);
     $this->Image(__DIR__ . "/../images/logo.png", 72 * 3);
     $this->AddPage();
   }
 
+  public function withSmartBreak($generator) {
+
+    $scratchpad = new Book();
+    $generator($scratchpad);
+    $height = $scratchpad->GetY();
+    $remaining = $this->GetPageHeight() - $this->GetY() - $this->margins['bottom'];
+
+    if($height > $remaining) {
+      $this->AddPage();
+    }
+
+    $generator($this);
+  }
+
+  function withIndent($size, $generator) {
+    $this->SetX($this->margins['left']+ $size);
+    $generator();
+
+    $this->SetX($this->margins['left']);
+  }
+
   public function addEntry($entry) {
-    $this->withStyle('h1', function() use($entry){
-      $this->Cell(0, 16, $entry['The Count'], 0, 2);
-    });
+    $this->withSmartBreak(function($book) use($entry) {
+      $book->withStyle('h1', function() use($entry,$book){
+        $book->Cell(0, 16, $entry['The Count'], 0, 2);
+      });
 
-    $this->withStyle('h2', function() use($entry) {
-      $this->Cell(0, 20, $entry['Topic'], 0, 2);
-    });
+      $book->withStyle('h2', function() use($entry, $book) {
+        $book->Cell(0, 20, $entry['Topic'], 0, 2);
+      });
 
-    $y_before = $this->GetY();
+      $y_before = $book->GetY();
 
-    $this->SetLeftMargin(45);
-    $this->SetRightMargin(45);
-    $this->MultiCell(0, 16, $this->scrub($entry['Content']), 0, 'L');
-    $this->SetLineWidth(3);
-    $this->SetDrawColor(200, 200, 200);
+      $this->withIndent(15, function() use($book, $entry) {
+        $book->MultiCell(0, 16, $book->scrub($entry['Content']), 0, 'L');
+      });
 
-    if($url = trim($entry['Learn More'])) {
-      $short_url = bitly_shrink($url);
-      $label = str_replace('https://', '', $short_url);
-      $text = "Read More at $label";
       $this->Ln();
-      $this->Cell($this->GetStringWidth($text), 14, $text, '', 2, 'C', false, $url);
-    }
-    $y_after = $this->GetY();
-    $this->SetLeftMargin(36);
-    $this->SetRightMargin(36);
 
-    if($y_before < $y_after) {
-      $this->Line(40, $y_before, 40, $y_after);
-    }
+      $this->withIndent(15, function() use ($book, $entry) {
+        if($url = trim($entry['Learn More'])) {
+          $short_url = bitly_shrink($url);
+          $label = str_replace('https://', '', $short_url);
+          $text = "Read More at $label";
+          $book->Cell($book->GetStringWidth($text), 14, $text, '', 2, 'C', false, $url);
+        }
+      });
 
-    $this->Ln();
-    $this->Ln();
+      $y_after = $book->GetY();
+
+      if($y_before < $y_after) {
+        $book->SetLineWidth(3);
+        $book->SetDrawColor(200, 200, 200);
+        $book->Line($this->margins['left'] + 4, $y_before, $this->margins['left'] + 4, $y_after);
+      }
+      $book->Ln();
+      $book->Ln();
+    });
   }
 
   public function setStyle($name) {
@@ -103,7 +135,8 @@ class Book extends FPDF {
     $fixes = [
       "’" => "'",
       '“' =>  '"',
-      '”' =>  '”'
+      '”' =>  '"',
+      '–' => '-',
     ];
 
     return str_replace(array_keys($fixes), array_values($fixes), $text);
